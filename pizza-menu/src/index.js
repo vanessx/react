c50e87a4a8a0feb44c208663011ecd3cb3858cab
vanessx <vanessafoxandrade@hotmail.com>
2024-12-30 23:51:13 +0100

    some changes

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,8 +1,102 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+	{
+		name: "Focaccia",
+		ingredients: "Bread with italian olive oil and rosemary",
+		price: 6,
+		photoName: "pizzas/focaccia.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Margherita",
+		ingredients: "Tomato and mozarella",
+		price: 10,
+		photoName: "pizzas/margherita.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Spinaci",
+		ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+		price: 12,
+		photoName: "pizzas/spinaci.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Funghi",
+		ingredients: "Tomato, mozarella, mushrooms, and onion",
+		price: 12,
+		photoName: "pizzas/funghi.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Salamino",
+		ingredients: "Tomato, mozarella, and pepperoni",
+		price: 15,
+		photoName: "pizzas/salamino.jpg",
+		soldOut: true,
+	},
+	{
+		name: "Pizza Prosciutto",
+		ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+		price: 18,
+		photoName: "pizzas/prosciutto.jpg",
+		soldOut: false,
+	},
+];
 
 function App() {
-	return <h1>Hello React!</h1>;
+	return (
+		<div className="container">
+			<Header />
+			<Menu />
+			<Footer />
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<header className="header">
+			<h1>Fast React Pizza Co.</h1>
+		</header>
+	);
+}
+
+function Menu() {
+	return (
+		<main className="menu">
+			<h2>Our menu</h2>
+			<ul className="pizzas">
+				{pizzaData.map((pizza) => (
+					<Pizza pizzaObj={pizza} key={pizza.name} />
+				))}
+			</ul>
+		</main>
+	);
+}
+
+function Pizza(props) {
+	return (
+		<li className="pizza">
+			<img src={props.pizzaObj.photoName} alt="spinaci pizza"></img>
+			<div>
+				<h3>{props.pizzaObj.name}</h3>
+				<p>{props.pizzaObj.ingredients}</p>
+				<span>{props.pizzaObj.price}</span>
+			</div>
+		</li>
+	);
+}
+
+function Footer() {
+	return (
+		<footer className="footer">
+			{new Date().toLocaleTimeString()}. We're currently open
+		</footer>
+	);
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
