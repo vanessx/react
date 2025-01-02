738f47dc793600c4794ecaaab08e332def5cd030
vanessx <vanessafoxandrade@hotmail.com>
2025-01-02 13:21:36 +0100

    final commit

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -66,36 +66,71 @@ function Header() {
 }
 
 function Menu() {
+	const pizzas = pizzaData;
+	const numPizzas = pizzas.length;
 	return (
 		<main className="menu">
 			<h2>Our menu</h2>
-			<ul className="pizzas">
-				{pizzaData.map((pizza) => (
-					<Pizza pizzaObj={pizza} key={pizza.name} />
-				))}
-			</ul>
+			{numPizzas > 0 ? (
+				<>
+					<p>
+						Authentic Italian cuisine. 6 creative dishes to choose from. All
+						from our stone oven, all organic, all delicious.
+					</p>
+					<ul className="pizzas">
+						{pizzas.map((pizza) => (
+							<Pizza pizzaObj={pizza} key={pizza.name} />
+						))}
+					</ul>
+				</>
+			) : (
+				<p>We're still working on our menu.</p>
+			)}
 		</main>
 	);
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
 	return (
-		<li className="pizza">
-			<img src={props.pizzaObj.photoName} alt="spinaci pizza"></img>
+		<li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+			<img src={pizzaObj.photoName} alt="spinaci pizza"></img>
 			<div>
-				<h3>{props.pizzaObj.name}</h3>
-				<p>{props.pizzaObj.ingredients}</p>
-				<span>{props.pizzaObj.price}</span>
+				<h3>{pizzaObj.name}</h3>
+				<p>{pizzaObj.ingredients}</p>
+				<span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
 			</div>
 		</li>
 	);
 }
 
 function Footer() {
+	const hour = new Date().getHours();
+	const openHour = 12;
+	const closeHour = 22;
+	const isOpen = hour >= openHour && hour <= closeHour;
+
 	return (
 		<footer className="footer">
-			{new Date().toLocaleTimeString()}. We're currently open
+			{isOpen ? (
+				<Order closeHour={closeHour} openHour={openHour} />
+			) : (
+				<p>
+					We're happy welcome you between {openHour}:00 and {closeHour}:00.
+				</p>
+			)}
 		</footer>
+	);
+}
+
+function Order({ closeHour, openHour }) {
+	return (
+		<div className="order">
+			<p>
+				We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+				online.
+			</p>
+			<button className="btn">Order</button>
+		</div>
 	);
 }
 
