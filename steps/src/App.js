9f388eb6d7104e1e58d24fd1d3d090c724f9a6d2
vanessx <vanessafoxandrade@hotmail.com>
2025-01-07 20:02:30 +0100

    final commit

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const messages = [
-	"Learn React ⚛️",
-	"Apply for jobs 💼",
-	"Invest your new income 🤑",
+	'Learn React ⚛️',
+	'Apply for jobs 💼',
+	'Invest your new income 🤑',
 ];
 
 export default function App() {
+	return (
+		<div>
+			<Step />
+		</div>
+	);
+}
+
+function Step() {
 	const [step, setStep] = useState(1);
 	const [isOpen, setIsOpen] = useState(true);
 
@@ -26,29 +34,48 @@ export default function App() {
 			{isOpen && (
 				<div className="steps">
 					<div className="numbers">
-						<div className={step >= 1 && "active"}>1</div>
-						<div className={step >= 2 && "active"}>2</div>
-						<div className={step >= 3 && "active"}>3</div>
+						<div className={step >= 1 && 'active'}>1</div>
+						<div className={step >= 2 && 'active'}>2</div>
+						<div className={step >= 3 && 'active'}>3</div>
 					</div>
-					<p className="message">
-						Step {step}: {messages[step - 1]}
-					</p>
+					<StepMessage step={step}>{messages[step - 1]}</StepMessage>
 					<div className="buttons">
-						<button
-							style={{ backgroundColor: "#7950f2", color: "#fff" }}
+						<Button
+							bgColor="#7950f2"
+							textColor="#fff"
 							onClick={handlePrevious}
+							text="Previous"
+							emoji="👈🏻"
 						>
-							Previous
-						</button>
-						<button
-							style={{ backgroundColor: "#7950f2", color: "#fff" }}
-							onClick={handleNext}
-						>
-							Next
-						</button>
+							<span>👈🏻</span> Previous
+						</Button>
+						<Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+							Next <span>👉🏻</span>
+						</Button>
 					</div>
 				</div>
 			)}
 		</>
+	);
+}
+
+function StepMessage({ step, children }) {
+	return (
+		<div className="message">
+			<h3>Step {step}:</h3>
+			{children}
+		</div>
+	);
+}
+
+function Button({ bgColor, textColor, onClick, children }) {
+	return (
+		<button
+			style={{ backgroundColor: bgColor, color: textColor }}
+			onClick={onClick}
+		>
+			{children}{' '}
+			{/* children props to get the content between the open and close of Button's component */}
+		</button>
 	);
 }
