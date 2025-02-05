a05f8cd12339d91e0bdee7883daecad0b4d4065b
vanessx <vanessafoxandrade@hotmail.com>
2025-02-05 14:52:45 +0100

    last commit

--- use-popcorn/src/App.js
+++ use-popcorn/src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import StarRating from './StarRating';
 import { useMovies } from './useMovies';
+import { useLocalStorageState } from './useLocalStorageState';
+import { useKey } from './useKey';
 
 const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -11,11 +13,7 @@ export default function App() {
 	const [query, setQuery] = useState('');
 	const [selectedId, setSelectedId] = useState(null);
 	const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
-	// const [watched, setWatched] = useState([]);
-	const [watched, setWatched] = useState(function () {
-		const storedValue = localStorage.getItem('watched');
-		return JSON.parse(storedValue);
-	});
+	const [watched, setWatched] = useLocalStorageState([], 'watched');
 
 	function handleSelectMovie(id) {
 		setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -32,13 +30,6 @@ export default function App() {
 	function handleDeleteWatched(id) {
 		setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
 	}
-
-	useEffect(
-		function () {
-			localStorage.setItem('watched', JSON.stringify(watched));
-		},
-		[watched]
-	);
 
 	return (
 		<>
@@ -119,26 +110,11 @@ function Logo() {
 function Search({ query, setQuery }) {
 	const inputEl = useRef(null);
 
-	useEffect(
-		function () {
-			function callback(e) {
-				if (document.activeElement === inputEl.current) return;
-
-				if (e.code === 'Enter') {
-					inputEl.current.focus();
-					setQuery('');
-				}
-			}
-
-			document.addEventListener('keydown', callback);
-			inputEl.current.focus();
-
-			return () => {
-				document.removeEventListener('keydown', callback);
-			};
-		},
-		[setQuery]
-	);
+	useKey('Enter', function () {
+		if (document.activeElement === inputEl.current) return;
+		inputEl.current.focus();
+		setQuery('');
+	});
 
 	return (
 		<input
@@ -164,28 +140,7 @@ function Box({ children }) {
 		</div>
 	);
 }
-/*
-function WatchedBox() {
-	const [isOpen2, setIsOpen2] = useState(true);
 
-	return (
-		<div className="box">
-			<button
-				className="btn-toggle"
-				onClick={() => setIsOpen2((open) => !open)}
-			>
-				{isOpen2 ? '–' : '+'}
-			</button>
-			{isOpen2 && (
-				<>
-					<WatchedSummary watched={watched} />
-					<WatchedMovieList watched={watched} />
-				</>
-			)}
-		</div>
-	);
-}
-*/
 function MovieList({ movies, onSelectMovie }) {
 	return (
 		<ul className="list list-movies">
@@ -259,21 +214,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 		onCloseMovie();
 	}
 
-	useEffect(
-		function () {
-			function callback(e) {
-				if (e.code === 'Escape') {
-					onCloseMovie();
-				}
-			}
-			document.addEventListener('keydown', callback);
-
-			return function () {
-				document.removeEventListener('keydown', callback);
-			};
-		},
-		[onCloseMovie]
-	);
+	useKey('Escape', onCloseMovie);
 
 	useEffect(
 		function () {
