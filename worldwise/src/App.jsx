721f1a4355ee1bc673b6c6e6cb6bb12bc6166804
vanessx <vanessafoxandrade@hotmail.com>
2025-03-17 11:40:59 +0100

    add cities info

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -11,31 +10,11 @@ import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
-
-const BASE_URL = 'http://localhost:8000';
+import { CitiesProvider } from './contexts/CitiesContext';
 
 function App() {
-	const [cities, setCities] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-
-	useEffect(function () {
-		async function fetchCities() {
-			try {
-				setIsLoading(true);
-				const res = await fetch(`${BASE_URL}/cities`);
-				const data = await res.json();
-				setCities(data);
-			} catch {
-				alert('There was an error loading data...');
-			} finally {
-				setIsLoading(false);
-			}
-		}
-		fetchCities();
-	}, []);
-
 	return (
-		<div>
+		<CitiesProvider>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Homepage />} />
@@ -46,19 +25,19 @@ function App() {
 						<Route index element={<Navigate replace to="cities" />} />
 						<Route
 							path="cities"
-							element={<CityList cities={cities} isLoading={isLoading} />}
+							element={<CityList />}
 						/>
 						<Route path="cities/:id" element={<City />} />
 						<Route
 							path="countries"
-							element={<CountryList cities={cities} isLoading={isLoading} />}
+							element={<CountryList />}
 						/>
 						<Route path="form" element={<Form />} />
 					</Route>
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-		</div>
+		</CitiesProvider>
 	);
 }
 
--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,78 +1,68 @@
-// import styles from "./City.module.css";
+import styles from "./City.module.css";
 
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useCities } from '../contexts/CitiesContext';
+import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
-// const formatDate = (date) =>
-//   new Intl.DateTimeFormat("en", {
-//     day: "numeric",
-//     month: "long",
-//     year: "numeric",
-//     weekday: "long",
-//   }).format(new Date(date));
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  }).format(new Date(date));
 
 function City() {
 	const { id } = useParams(); // returns the param in this case is the id
-	const [searchParams, setSearchParams] = useSearchParams();
-	const lat = searchParams.get('lat');
-	const lng = searchParams.get('lng');
-
-	// TEMP DATA
-	const currentCity = {
-		cityName: 'Lisbon',
-		emoji: '🇵🇹',
-		date: '2027-10-31T15:59:59.138Z',
-		notes: 'My favorite city so far!',
-	};
+	const { getCity, currentCity, isLoading } = useCities()
+	
+	useEffect(function () {
+		getCity(id)
+	}, [id])
 
 	const { cityName, emoji, date, notes } = currentCity;
-	console.log(cityName, emoji, date, notes);
+
+	if (isLoading) return <Spinner />
 
 	return (
-		<>
-			<button onClick={() => setSearchParams({ lat: 23, lng: 50 })}></button>
-			<h1>
-				City {id}, {lat}, {lng}
-			</h1>
-			;
-		</>
+	  <div className={styles.city}>
+	    <div className={styles.row}>
+	      <h6>City name</h6>
+	      <h3>
+	        <span>{emoji}</span> {cityName}
+	      </h3>
+	    </div>
+
+	    <div className={styles.row}>
+	      <h6>You went to {cityName} on</h6>
+	      <p>{formatDate(date || null)}</p>
+	    </div>
+
+	    {notes && (
+	      <div className={styles.row}>
+	        <h6>Your notes</h6>
+	        <p>{notes}</p>
+	      </div>
+	    )}
+
+	    <div className={styles.row}>
+	      <h6>Learn more</h6>
+	      <a
+	        href={`https://en.wikipedia.org/wiki/${cityName}`}
+	        target="_blank"
+	        rel="noreferrer"
+	      >
+	        Check out {cityName} on Wikipedia &rarr;
+	      </a>
+	    </div>
+
+			<div>
+				<BackButton />
+	    </div>
+	  </div>
 	);
-
-	// return (
-	//   <div className={styles.city}>
-	//     <div className={styles.row}>
-	//       <h6>City name</h6>
-	//       <h3>
-	//         <span>{emoji}</span> {cityName}
-	//       </h3>
-	//     </div>
-
-	//     <div className={styles.row}>
-	//       <h6>You went to {cityName} on</h6>
-	//       <p>{formatDate(date || null)}</p>
-	//     </div>
-
-	//     {notes && (
-	//       <div className={styles.row}>
-	//         <h6>Your notes</h6>
-	//         <p>{notes}</p>
-	//       </div>
-	//     )}
-
-	//     <div className={styles.row}>
-	//       <h6>Learn more</h6>
-	//       <a
-	//         href={`https://en.wikipedia.org/wiki/${cityName}`}
-	//         target="_blank"
-	//         rel="noreferrer"
-	//       >
-	//         Check out {cityName} on Wikipedia &rarr;
-	//       </a>
-	//     </div>
-
-	//     <div>
-	//     </div>
-	//   </div>
-	// );
 }
 
 export default City;
