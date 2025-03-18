1ef37e2e9e00b2d21f5ed2fedf520513aaf0a647
vanessx <vanessafoxandrade@hotmail.com>
2025-03-18 11:17:39 +0100

    add map

--- worldwise/src/components/BackButton.jsx
+++ worldwise/src/components/BackButton.jsx
@@ -5,14 +5,14 @@ function BackButton() {
   const navigate = useNavigate();
 
   return <Button
-					type="back"
-					onClick={(e) => {
-						e.preventDefault();
-						navigate(-1);
-					}}
-				>
-					&larr; Back
-				</Button>
+          type="back"
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(-1);
+          }}
+        >
+          &larr; Back
+        </Button>
 }
 
 export default BackButton
