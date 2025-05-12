c3c4a6f01beba0362a7f7c0f5757dcb41bff9163
vanessx <vanessafoxandrade@hotmail.com>
2025-05-12 22:33:16 +0200

    change user

--- worldwise/src/components/User.jsx
+++ worldwise/src/components/User.jsx
@@ -1,24 +1,24 @@
-import styles from "./User.module.css";
+import styles from './User.module.css';
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+	name: 'urassaya',
+	email: 'urassaya@example.com',
+	password: 'qwerty',
+	avatar: 'https://i.pravatar.cc/100?u=zz',
 };
 
 function User() {
-  const user = FAKE_USER;
+	const user = FAKE_USER;
 
-  function handleClick() {}
+	function handleClick() {}
 
-  return (
-    <div className={styles.user}>
-      <img src={user.avatar} alt={user.name} />
-      <span>Welcome, {user.name}</span>
-      <button onClick={handleClick}>Logout</button>
-    </div>
-  );
+	return (
+		<div className={styles.user}>
+			<img src={user.avatar} alt={user.name} />
+			<span>Welcome, {user.name}</span>
+			<button onClick={handleClick}>Logout</button>
+		</div>
+	);
 }
 
 export default User;
