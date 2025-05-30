25aeaab0f6d3ba40d8a60ee39d6db691a1d10840
vanessx <vanessafoxandrade@hotmail.com>
2025-05-30 10:46:48 +0200

    add redux

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -12,7 +14,12 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case 'account/requestLoan':
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case 'account/payLoan':
       return {
         ...state,
@@ -25,3 +32,26 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
