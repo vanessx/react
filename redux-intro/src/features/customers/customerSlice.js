5e7cb6019b449ef70caf870b5e96304b38394d8c
vanessx <vanessafoxandrade@hotmail.com>
2025-06-05 11:51:42 +0200

    final commit

--- redux-intro/src/features/customers/customerSlice.js
+++ redux-intro/src/features/customers/customerSlice.js
@@ -1,11 +1,43 @@
-const initialStateCustomer = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   fullName: '',
   nationalID: '',
   createdAt: '',
 };
 
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
 
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/*
 export default function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
     case 'customer/createCustomer':
@@ -33,3 +65,4 @@ export function createCustomer(fullName, nationalID) {
 export function updateName(fullName) {
   return { type: 'customer/updateName', payload: fullName };
 }
+*/
