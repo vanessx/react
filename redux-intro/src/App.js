4b7818e2ea19d2bfa44bb7e3f402f7b92c666a26
vanessx <vanessafoxandrade@hotmail.com>
2025-06-05 11:14:18 +0200

    add redux toolkit

--- redux-intro/src/App.js
+++ redux-intro/src/App.js
@@ -5,16 +5,21 @@ import BalanceDisplay from './features/account/BalanceDisplay';
 import { useSelector } from 'react-redux';
 
 function App() {
-  const customer = useSelector(store => store.customer.fullName)
-  const accountBalance = useSelector(store => store.account.balance)
+  const customer = useSelector((store) => store.customer.fullName);
+  const accountBalance = useSelector((store) => store.account.balance);
 
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      {!customer && <CreateCustomer />}
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay balance={accountBalance}/>
+      {!customer ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay balance={accountBalance} />
+        </>
+      )}
     </div>
   );
 }
