d02fdffc75ea68d46ee7a20ba4c32b694c26ad85
vanessx <vanessafoxandrade@hotmail.com>
2025-06-10 10:52:25 +0200

    add more styles

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -46,13 +47,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -60,16 +61,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              /* prettier-ignore */
-              className="rounded-full border border-stone-200 px-4 py-2
-              text-sm transition-all duration-300 placeholder:text-stone-400
-              focus:outline-none focus:ring focus:ring-yellow-500 w-full
-              md:px-6 md:py-3"
-              type="text"
-              name="address"
-              required
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
@@ -88,16 +80,9 @@ function CreateOrder() {
         </div>
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            /* prettier-ignore */
-            className="inline-block bg-yellow-500 px-4 py-3 font-semibold 
-            uppercase text-stone-800 tracking-wide rounded-full hover:bg-yellow-400
-            transition-colors duration-300 focus:outline-none focus:ring
-            focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? 'Placing order...' : 'Order now'}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -8,13 +8,12 @@ function AppLayout() {
   const isLoading = navigation.state === 'loading';
 
   return (
-    /* prettier-ignore */
-    <div className="grid-rows-[auto_1fr_auto] grid h-screen">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
-      <div className='overflow-auto'>
-        <main className='max-w-3xl mx-auto'>
+      <div className="overflow-auto">
+        <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
       </div>
