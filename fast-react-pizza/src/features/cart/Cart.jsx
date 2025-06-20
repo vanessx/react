78dac10ae9471fc269354073b0a3f00428ca78fc
vanessx <vanessafoxandrade@hotmail.com>
2025-06-20 11:38:08 +0200

    styling menu

--- fast-react-pizza/src/features/cart/Cart.jsx
+++ fast-react-pizza/src/features/cart/Cart.jsx
@@ -34,7 +34,7 @@ function Cart() {
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
       <h2>Your cart, %NAME%</h2>
       <div>
-        <Button to="/order/new">Order pizzas</Button>
+        <Button to="/order/new" type='primary'>Order pizzas</Button>
         <button>Clear cart</button>
       </div>
     </div>
--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -1,20 +1,25 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
-  const className = `inline-block rounded-full bg-yellow-500 px-4 py-3 font-semibold 
+function Button({ children, disabled, to, type }) {
+  const base = `inline-block rounded-full bg-yellow-500 font-semibold 
     uppercase tracking-wide text-stone-800 transition-colors duration-300 
     hover:bg-yellow-400 focus:outline-none focus:ring focus:ring-yellow-300 
-    focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4`;
+    focus:ring-offset-2 disabled:cursor-not-allowed`;
+
+  const styles = {
+    primary: base + 'px-4 py-3 md:px-6 md:py-4',
+    small: base + 'py-2 md:px-5 md:py-2.5 text-xs',
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
