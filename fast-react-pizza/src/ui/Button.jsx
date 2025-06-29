d080caeb1f2bd29a794e7c0b560d3f934fe459d2
vanessx <vanessafoxandrade@hotmail.com>
2025-06-29 18:29:01 +0200

    finish styling

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type }) {
-  const base = `inline-block rounded-full bg-yellow-500 font-semibold 
+  const base = `inline-block text-sm rounded-full bg-yellow-500 font-semibold 
     uppercase tracking-wide text-stone-800 transition-colors duration-300 
     hover:bg-yellow-400 focus:outline-none focus:ring focus:ring-yellow-300 
     focus:ring-offset-2 disabled:cursor-not-allowed`;
 
   const styles = {
     primary: base + 'px-4 py-3 md:px-6 md:py-4',
-    secondary: `inline-block rounded-full border-2 vorder-stone-300 font-semibold 
+    secondary: `text-sm inline-block rounded-full border-2 vorder-stone-300 font-semibold 
     uppercase tracking-wide text-stone-400 transition-colors duration-300 
     hover:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 
     focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5
