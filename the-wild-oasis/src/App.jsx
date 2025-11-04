579ff32bfdf3378d207b6427852d4cf55a0c994f
vanessx <vanessafoxandrade@hotmail.com>
2025-11-04 10:50:31 +0100

    css function

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,27 +1,8 @@
 import styled from 'styled-components';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0%.8rem 1.2rem;
-`;
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
 
 const StyledApp = styled.main`
   background-color: orangered;
@@ -30,11 +11,16 @@ const StyledApp = styled.main`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert('checked in')}>Check in</Button>
-      <Input type="number" placeholder="Number of guests"></Input>
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading type="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check</Heading>
+        <Button onClick={() => alert('checked in')}>Check in</Button>
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
   );
 }
 
