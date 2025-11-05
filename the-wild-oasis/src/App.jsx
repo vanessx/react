fb8965895939c070538f870b5222eaec66e41eea
vanessx <vanessafoxandrade@hotmail.com>
2025-11-05 10:41:44 +0100

    reusable styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.main`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -14,11 +14,29 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading type="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check</Heading>
-        <Button onClick={() => alert('checked in')}>Check in</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests"></Input>
+        <Row>
+          <Row type="horizontal">
+            <Heading type="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check</Heading>
+              <Button onClick={() => alert('checked in')}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert('checked out')}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests"></Input>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
